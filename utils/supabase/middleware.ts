dc3a8036fb25df527cdd13bf2553f454ddd89036
Devvrat Gupta <158533901+mrdev212@users.noreply.github.com>
2026-07-10 00:48:35 +0530

    Safely bypass Supabase session update in middleware if env variables are not configured

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,9 +1,6 @@
 import { createServerClient } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
 export const updateSession = async (request: NextRequest) => {
   // Create an unmodified response
   let supabaseResponse = NextResponse.next({
@@ -11,6 +8,13 @@ export const updateSession = async (request: NextRequest) => {
       headers: request.headers,
     },
   });
+
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!supabaseUrl || !supabaseKey) {
+    return supabaseResponse;
+  }
 
   const supabase = createServerClient(
     supabaseUrl!,
